91061e7632fdc0e029ae81bc0b8b838bd2f5b313
GuilhermeSa <guilherme.sa@catho.com>
2019-03-20 09:13:22 -0300

    feat: add dependencies example

--- main.tf
+++ main.tf
@@ -1,21 +1,56 @@
-// O provider é responsável por entender as interações da API e expôr recursos.
-// Providers geralmente são IaaS (ex: AWS, GCP, Azure), SaaS (ex: Heroku) ou PaaS (ex: Cloudflare)
-// Mais info em: https://www.terraform.io/docs/providers/index.html
 provider "aws" {
   # access_key = "ACCESS_KEY_HERE"
   # secret_key = "SECRET_KEY_HERE"
   region = "us-east-1"
 }
 
-// Recursos são os elementos mais importantes do Terraform.
-// Cada bloco de recurso descreve um ou mais objetos de infraestrutura, como
-// redes, instâncias, etc
-resource "aws_instance" "sample_instance" {
+// Por padrão o terraform infere as dependências de acordo com as expressões utilizadas
+data "aws_ami" "amz_ec2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-*"]
+  }
+
+  owners = ["591542846629"] # AWS
+}
+
+resource "aws_instance" "instance_a" {
+  ami           = "${data.aws_ami.amz_ec2.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Instance-A"
+  }
+}
+
+// Nem sempre é possível inferir, nesses casos o “depends_on” pode ser utilizado
+resource "aws_instance" "instance_b" {
+  ami           = "${data.aws_ami.amz_ec2.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Instance-B"
+  }
+
+  depends_on = ["aws_instance.instance_a"]
+}
+
+// Recursos sem depêndencias são criados em paralelo
+resource "aws_instance" "instance_c" {
   ami = "ami-0de53d8956e8dcf80"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
-    Key  = "Value"
+    Name = "Instance-C"
+  }
+}
+resource "aws_instance" "instance_d" {
+  ami = "ami-0de53d8956e8dcf80"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Instance-D"
   }
 }
