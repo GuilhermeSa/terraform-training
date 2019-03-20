0604abc5564cedb0d6e6e5c7e3cb38fc905ef346
GuilhermeSa <guilherme.sa@catho.com>
2019-03-20 13:49:14 -0300

    style(provisioners): change comment format

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-// Por padrão o terraform infere as dependências de acordo com as expressões utilizadas
+# Por padrão o terraform infere as dependências de acordo com as expressões utilizadas
 data "aws_ami" "amz_ec2" {
   most_recent = true
 
@@ -16,7 +16,7 @@ data "aws_ami" "amz_ec2" {
   }
 }
 
-// Provisioners são usados no momento da criação de recursos, quando é necessário executar algum tipo de configuração inicial como transferir arquivos ou executar algum script ou ferramenta.
+# Provisioners são usados no momento da criação de recursos, quando é necessário executar algum tipo de configuração inicial como transferir arquivos ou executar algum script ou ferramenta.
 resource "aws_instance" "instance_a" {
   ami           = "${data.aws_ami.amz_ec2.id}"
   instance_type = "t2.micro"
@@ -30,8 +30,8 @@ resource "aws_instance" "instance_a" {
   }
 }
 
-// Se um provisioner falha durante a criação, o recurso é criado porém é marcado como tainted pelo Terraform.
-// Durante o próximo apply, recursos marcados como tainted são recriados.
+# Se um provisioner falha durante a criação, o recurso é criado porém é marcado como tainted pelo Terraform.
+# Durante o próximo apply, recursos marcados como tainted são recriados.
 resource "aws_instance" "instance_b" {
   ami           = "${data.aws_ami.amz_ec2.id}"
   instance_type = "t2.micro"
@@ -45,10 +45,10 @@ resource "aws_instance" "instance_b" {
   }
 }
 
-// Provisioners podem ser especificados para serem executados no momento do destroy.
-// Esses provisioners são executados antes do recurso ser destruído.
-// Provisioners de destroy só são executados caso a configuração do recurso ainda exista nos arquivos do Terraform, se o recurso for removido do arquivo o provisioner não é executado.
-// Mais informações em: https://www.terraform.io/docs/provisioners/
+# Provisioners podem ser especificados para serem executados no momento do destroy.
+# Esses provisioners são executados antes do recurso ser destruído.
+# Provisioners de destroy só são executados caso a configuração do recurso ainda exista nos arquivos do Terraform, se o recurso for removido do arquivo o provisioner não é executado.
+# Mais informações em: https://www.terraform.io/docs/provisioners/
 resource "aws_instance" "instance_c" {
   ami           = "${data.aws_ami.amz_ec2.id}"
   instance_type = "t2.micro"
